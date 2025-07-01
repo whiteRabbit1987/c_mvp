305036a20e1413c4e6254076d7cb76f7e527123f
whiteRabbit87 <davidstephenmoseley1987@gmail.com>
2025-07-01 10:29:05 -0500

    Variable practise

--- basics/variables_and_types.c
+++ basics/variables_and_types.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int x; //declaration
-    x = 123; //initialization
+    // variable = A reusable container for a value.
+    //            Behaves as if it were the value it contians.
 
-    int y = 321; //declaration + initialization
-    int age = 21; //whole number
-    float gpa = 2.05; //floating point number
-    char grade = 'C'; //single character
-    char name[] = "Bro"; //array of characters (string)
+    int age; // declaration
+    age = 25; // initialization
+    float gpa = 2.5; //declaration + initialization
+    char grade = 'C';
+    char name[] = "David";
+    double pi = 3.14159265358979;
 
-    printf("Hello %s\n", name);
     printf("You are %d years old\n", age);
-    printf("Your average grade is %c\n", grade);
     printf("Your gpa is %.2f\n", gpa);
+    printf("Your grade is %c\n", grade);
+    printf("Hello %s\n", name);
+    printf("The value of pi is: %.15lf\n", pi);
 
     return 0;
 }
